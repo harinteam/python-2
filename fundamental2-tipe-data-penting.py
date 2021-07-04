d0ecd595904f510eeac935be8d708ebb8f61f915
adityadenny <adityadenny6@gmail.com>
2021-07-04 18:42:23 +0700

    initial commit

--- fundamental2-tipe-data-penting.py
+++ fundamental2-tipe-data-penting.py
@@ -9,4 +9,19 @@ print(anak2)
 print(anak3)
 print(anak4)
 
+print('---' * 7)
+
 # Tipe data List/daftar/array
+anak = ['Muhammad Umar Alfatih', 'Nyimas Hanin Asyifa Almahira', 'Muhammad Uwais Alqarni', 'Nyimas Husna Aghnia']
+print(anak)
+anak.append('Belum tau')
+print(anak)
+
+print('---' * 7)
+# Sapa anak ke-2
+print(f'Hai {anak[1]}!')
+
+print('---' * 7)
+# Sapa semua anak cara gampang
+for a in anak:
+    print(f'Hai {a}')
